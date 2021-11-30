76064da485853e5fa9ae3c0ccd7ea4f4968015bc
l-1_l <86021698+ihateallsociety@users.noreply.github.com>
2021-11-30 21:38:20 +0300

    !!!

--- main.py
+++ main.py
@@ -17,20 +17,47 @@ async def bot_start(message: types.Message):
     await bot.send_message(message.from_user.id, "Здравствуйте, чем могу вам помочь?", reply_markup=start_keyboard)
 
 
+async def range_production_func(message: types.Message):
+    await bot.send_message(message.from_user.id, "Ассортимент:", reply_markup=range_production_keyboard)
+
+
+async def one_pod_func(message: types.Message):
+    pass
+
+
+# функции, который возвращают на шаг назад
+async def back_to_start_func(message: types.Message):
+    await bot.send_message(message.from_user.id, 'Чем могу вам помочь?', reply_markup=start_keyboard)
+
 # -----------------------------ADMIN SIDE---------------------------------------
 
 # -----------------------------OTHER SIDE--------------------------------------
 
 # ----------------------------KEYBOARD SIDE-------------------------------------
 # начальная клавиатура
-trace_mail, range_production = KeyboardButton("Поставки в ближайшее время"), KeyboardButton("Ассортимент")
+trace_mail_btn, range_production_btn = KeyboardButton("Поставки в ближайшее время"), KeyboardButton("Ассортимент")
 start_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-start_keyboard.row(range_production).row(trace_mail)
+start_keyboard.row(range_production_btn).row(trace_mail_btn)
+# клавиаутура ассортимента
+hqd_btn, pod_btn, back_to_start_btn = KeyboardButton('Одноразки'), KeyboardButton('Поды'), KeyboardButton('Назад')
+range_production_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+range_production_keyboard.insert(hqd_btn).insert(pod_btn).row(back_to_start_btn)
+# клавиутура одноразок
+hqd_pod_btn, puff_pod_btn = KeyboardButton("Hqd"), KeyboardButton("Puff")
+time_pod_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+time_pod_keyboard.insert(hqd_pod_btn).insert(puff_pod_btn)
 
 
 # ----------------------------REGISTER FUNCS-------------------------------------
 def client_func(dp: Dispatcher):
     dp.register_message_handler(bot_start, commands='start')
+    dp.register_message_handler(range_production_func, lambda message: message.text == 'Ассортимент')
+    dp.register_message_handler(back_to_start_func, lambda message: message.text == 'Назад')
+    dp.register_message_handler(one_pod_func, lambda message: message.text in ['Hqd', 'Puff'])
+
+
+# ----------------------------CALL REG FUNCS-------------------------------------
+client_func(dp)
 
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
